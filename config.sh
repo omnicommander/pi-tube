cbaf93175cef501da2fb7a81b2676963b5ae39d0
Noah Beach <emailnoah@icloud.com>
2020-07-02 12:30:38 -0500

    Added project path to config

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
+PROJECTPATH="/home/pi/OC-DigitalSignage"
 JSONPATH="/home/pi/OC-DigitalSignage/json"          	    # path for json file from server.
 VIDEOPATH="/home/pi/OC-DigitalSignage/videos"                  # path for fetch to store video mp4 files
 fetchLog="/home/pi/OC-DigitalSignage/fetch.log"               # path for logging
