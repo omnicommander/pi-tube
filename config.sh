ecd237a409a4698fd20c448196c0f85875d7f55a
Noah Beach <emailnoah@icloud.com>
2020-07-27 13:27:49 -0500

    Whitespace to spaces

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 PROJECTPATH="/home/pi/OC-DigitalSignage"
-JSONPATH="/home/pi/OC-DigitalSignage/videos.json"          	    # path for json file from server.
+JSONPATH="/home/pi/OC-DigitalSignage/videos.json"               # path for json file from server.
 VIDEOPATH="/home/pi/OC-DigitalSignage/videos"                  # path for fetch to store video mp4 files
 fetchLog="/home/pi/OC-DigitalSignage/fetch.log"               # path for logging
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`               # timestamp formatting for logger
 URL="http://34.193.150.151/pi_callHome.php?id="     # url to server for requests
 AUTHOR="Eric Bradshaw bradshaw@omnicommander.com"      # author
-PI_ID="TestPi"   	                    # ID for this PI machine
+PI_ID="TestPi"                          # ID for this PI machine
 VERSION=1.4                                         # revision number
 BUCKETURL="https://digitalsignagebucket.s3.us-east-2.amazonaws.com" # URL for the bucket
