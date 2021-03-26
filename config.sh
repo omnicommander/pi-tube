64d5bb22b03b89ab0c0ef0b803c2c3a35d0fd1bc
Noah Beach <noah@omnicommander.com>
2021-03-26 16:23:53 -0500

    Made pi_id blank in configs

--- config.sh
+++ config.sh
@@ -6,5 +6,5 @@ fetchLog="/home/pi/OC-DigitalSignage/fetch.log"               # path for logging
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`               # timestamp formatting for logger
 URL="http://34.193.150.151/pi_callHome.php?id="     # url to server for requests
 AUTHOR="Eric Bradshaw bradshaw@omnicommander.com"      # author
-PI_UID="test-pi"   	                    # ID for this PI machine
+PI_UID=""   	                    # ID for this PI machine
 VERSION=1.6                                        # revision number
